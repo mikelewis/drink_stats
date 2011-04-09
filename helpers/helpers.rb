d3b3c0855122e968693e13e63102c98b9b09a2ed
Mike Lewis <ft.mikelewis@gmail.com>
2011-04-09 00:43:58 -0400

    Basic graph framework setup

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -10,5 +10,16 @@ module DrinkStats
     def debug_mysql(result)
       result.to_a
     end
+    def generate_graph_items(arr, key, options={})
+      options[:integer] ||= false
+      options[:quoted] ||= false
+      options[:link] ||= false
+      arr.map do |d|
+        el = (options[:qouted]) ? "\"#{d[key]}\"" : d[key]
+        el = el.to_i if options[:integer]
+        el = "#{"/" if options[:link] != ""}#{options[:link]}/#{el}" if options[:link]
+        el
+      end
+    end
   end
 end
